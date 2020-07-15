3f1fafdd6ad44b602b82a26398adb3f5703c2619
Ikraam <ikraamghoor@gmail.com>
2020-07-15 15:03:05 +0200

    game logic without win condition

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,35 +1,54 @@
 const testBoard = ['X', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O'];
+testBoard.fill('');
+let moveCount = 0;
+const wA = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-// const listeners = ()
 
+const hasWon = (playerArray) => wA.includes(playerArray);
 const Player = (name, symbol) => {
-   return {name, symbol}
-}
+  const arr = [];
+  return { name, symbol, arr };
+};
 
-player1 = Player("shabab","X")
-player2 = Player("ikraam","O")
+const player1 = Player('shabab', 'X');
+const player2 = Player('ikraam', 'O');
 
-const addListener = (element,method) => {
-    element.addEventListner('click',method)
-}
+// const addListener = (element, method) => {
+//   element.addEventListner('click', method);
+// };
+
+
+const playerOutput = () => {
+  moveCount += 1;
+  if (moveCount % 2 === 0) {
+    return player1;
+  }
+  return player2;
+};
+
+const playerMove = (index) => {
+  if (testBoard[index] === '') {
+    const currentPlayer = playerOutput();
+    document.querySelector(`.block${index + 1}`).firstChild.textContent = currentPlayer.symbol;
+    testBoard[index] = currentPlayer.symbol;
+    currentPlayer.arr.push(index);
+    if (hasWon(currentPlayer.arr)) {
+      window.alert(`${currentPlayer.name} has won!`);
+    }
+  } else {
+    window.alert('Please select an open block.');
+  }
+};
 
 const renderBoard = (board) => {
   board.forEach((letter, index) => {
     const block = document.querySelector(`.block${index + 1}`);
-    console.log(block);
-    block.addEventListener('click',()=>{playerMove(index)})
+    block.addEventListener('click', () => { playerMove(index); });
     const span = document.createElement('span');
     span.textContent = letter;
     block.append(span);
   });
 };
 
-
-
-const playerMove = (index) => {
-   const playerInput = ''
-   document.querySelector(`.block${index + 1}`).firstChild.textContent ='V'
-   testBoard[index] = 'V'
-}
 
 renderBoard(testBoard);
