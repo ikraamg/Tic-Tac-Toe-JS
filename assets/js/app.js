81ab77c5eb719053d98aef7d1bd527c6bb6612d8
Ikraam <ikraamghoor@gmail.com>
2020-07-16 18:04:47 +0200

    Refactored player setup to GameRound

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,19 +18,33 @@ const GameBoard = (() => {
   const testBoard = ['', '', '', '', '', '', '', '', ''];
   const gamesPlayed = 0;
   const wA = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  const resetBoard = function () { this.testBoard = ['', '', '', '', '', '', '', '', '']; };
   return {
-    testBoard, gamesPlayed, wA,
+    testBoard, gamesPlayed, wA, resetBoard,
   };
 })();
 
 const GameRound = () => {
   const moveCount = 0;
-  
+  let player1;
+  let player2;
+
+  const setPlayerNames = function () {
+    player1 = Player(document.querySelector('#player1').value, 'X');
+    player2 = Player(document.querySelector('#player2').value, 'O');
+  };
+
+  const resetPlayerArrays = function () {
+    player1.arr = [];
+    player2.arr = [];
+  };
+
   const isDraw = function () {
-    console.log(this, "this in gameround")
+    console.log(this, 'this in gameround');
     console.log('isDraw -> moveCount', this.moveCount);
     return this.moveCount > 8;
   };
+
   const getCurrentPlayer = function (player1, player2) {
     this.moveCount += 1;
     if (this.moveCount % 2 === 0) {
@@ -38,9 +52,10 @@ const GameRound = () => {
     }
     return player2;
   };
-  const playerMove = function (index, displayController, player1, player2, initializeGame) {
+
+  const playerMove = function (index, displayController, initializeGame) {
     if (GameBoard.testBoard[index] === '') {
-      console.log(this, 'in player move')
+      console.log(this, 'in player move');
       const currentPlayer = this.getCurrentPlayer(player1, player2);
       const currentBlock = document.querySelector(`.block${index + 1}`);
       displayController.replaceText(currentBlock, currentPlayer.symbol);
@@ -60,7 +75,7 @@ const GameRound = () => {
   };
 
   return {
-    moveCount, isDraw, getCurrentPlayer, playerMove,
+    moveCount, isDraw, getCurrentPlayer, playerMove, setPlayerNames, player1, player2, resetPlayerArrays,
   };
 };
 
--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,17 +1,14 @@
 // eslint-disable-next-line import/extensions
-import { Player, GameBoard, GameRound } from './app.js';
+import { GameBoard, GameRound } from './app.js';
 
-let player1;
-let player2;
 const round = GameRound();
 
-
 const displayController = (() => {
-  const renderBoard = (board) => {
-    board.forEach((letter, index) => {
+  const renderBoard = () => {
+    GameBoard.testBoard.forEach((letter, index) => {
       const block = document.querySelector(`.block${index + 1}`);
       if (GameBoard.gamesPlayed === 0) {
-        block.addEventListener('click', () => round.playerMove(index, displayController, player1, player2, initializeGame));
+        block.addEventListener('click', () => round.playerMove(index, displayController, initializeGame));
       }
       block.textContent = letter;
     });
@@ -29,21 +26,17 @@ const displayController = (() => {
   return { renderBoard, btnListners, replaceText };
 })();
 
+
 const initializeGame = () => {
-  GameBoard.testBoard = ['', '', '', '', '', '', '', '', ''];
+  GameBoard.resetBoard();
   if (GameBoard.gamesPlayed === 0) {
-    const name1 = document.querySelector('#player1').value;
-    const name2 = document.querySelector('#player2').value;
-    player1 = Player(name1, 'X');
-    player2 = Player(name2, 'O');
+    round.setPlayerNames();
   }
-  player1.arr = [];
-  player2.arr = [];
+  round.resetPlayerArrays();
   round.moveCount = 0;
-  displayController.renderBoard(GameBoard.testBoard);
+  displayController.renderBoard();
   GameBoard.gamesPlayed += 1;
   console.log('Game started');
-  return { player1, player2 };
 };
 
 displayController.btnListners();
