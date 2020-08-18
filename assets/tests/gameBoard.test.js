cc22d926d00a71bc109987cf38bccbf8b3a46a10
Ikraam <ikraamghoor@gmail.com>
2020-08-18 16:35:50 +0200

    Add tests for gameRound and gameBoard

---
+++ assets/tests/gameBoard.test.js
@@ -0,0 +1,13 @@
+import { GameBoard } from '../js/app';
+
+
+GameBoard.testBoard = ['1', '2', '', '', '', '', '', '', '']
+
+test('it ensures testBoard is not reset', () => {
+  expect(GameBoard.testBoard).not.toEqual(['', '', '', '', '', '', '', '', '']);
+});
+
+test('it tests if resetBoard has reset the board correctly', () => {
+  GameBoard.resetBoard();
+  expect(GameBoard.testBoard).toEqual(['', '', '', '', '', '', '', '', '']);
+});
